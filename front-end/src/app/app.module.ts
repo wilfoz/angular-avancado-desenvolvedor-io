ad7f27969a2283a29fbeb367ddf564877ac1dd4f
wilfoz <wil_foz@yahoo.com.br>
2020-10-29 11:05:19 -0300

    first commit

---
+++ front-end/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrModule } from 'ngx-toastr';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavegacaoModule } from './navegacao/navegacao.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NavegacaoModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot() 
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
